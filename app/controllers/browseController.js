ac8dc881fa5f9e47e589f3da9ca59eea195a853d
jtreleaven <jtreleaven262@gmail.com>
2015-03-24 19:22:31 -0400

    Added more IP Address stuff.

--- app/controllers/browseController.js
+++ app/controllers/browseController.js
@@ -2,6 +2,31 @@
 
 angcelor.controller("browseCtrl", ['$scope', 'Subnet', function($scope, Subnet) {
 
-    $scope.subnets = Subnet.query();
+    function initializeSubnets() {
+        var subnets = Subnet.query();
+        subnets[0]['selected'] = true;
+        for (var i = 1; i < subnets.length; i++) {
+            subnets[i]['selected'] = false;
+        }
+
+        return subnets;
+    }
+
+    function initializeIPAddresses(subnets) {
+        var ip_addresses = [];
+        for (subnet in subnets) {
+            if (subnet['selected']) {
+                ip_addresses.push(subnet.ip_addresses);
+            }
+        }
+
+        return ip_addresses;
+    }
+
+    var subnets = initializeSubnets();
+    var ip_addrs = initializeIPAddresses(subnets);
+
+    $scope.subnets = subnets;
+    $scope.ip_addrs = ip_addrs;
 
 }]);
