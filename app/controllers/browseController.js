5676e8c1ec908960f150aa7a0e89219c019e8724
jtreleaven <jtreleaven262@gmail.com>
2015-03-25 14:44:01 -0400

    Fixing some of the promise logic.

--- app/controllers/browseController.js
+++ app/controllers/browseController.js
@@ -4,29 +4,28 @@ angcelor.controller("browseCtrl", ['$scope', 'Subnet', function($scope, Subnet) 
 
     function initializeSubnets() {
         var subnets = Subnet.query();
-        subnets[0]['selected'] = true;
-        for (var i = 1; i < subnets.length; i++) {
-            subnets[i]['selected'] = false;
-        }
-
-        return subnets;
+        subnets.$promise.then(function(results) {
+            console.log(results);
+            results[0].selected = true;
+            for (var i = 1; i < results.length; i++) {
+                results[i].selected = false;
+            }
+            $scope.subnets = results;
+            initializeIPAddresses($scope.subnets);
+        });
     }
 
     function initializeIPAddresses(subnets) {
         var ip_addresses = [];
         for (subnet in subnets) {
-            if (subnet['selected']) {
+            if (subnet.selected) {
                 ip_addresses.push(subnet.ip_addresses);
             }
         }
 
-        return ip_addresses;
+        $scope.ip_addrs = ip_addresses;
     }
 
-    var subnets = initializeSubnets();
-    var ip_addrs = initializeIPAddresses(subnets);
-
-    $scope.subnets = subnets;
-    $scope.ip_addrs = ip_addrs;
+    initializeSubnets();
 
 }]);
