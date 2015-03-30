6d93dd01684f3f2b7c5ca62e143b02e1862ad977
jtreleaven <jtreleaven262@gmail.com>
2015-03-30 16:10:15 -0400

    Added stub directive file for splitter.

---
+++ app/directives/tableSplitter.js
@@ -0,0 +1,10 @@
+
+angcelor.directive('tableSplitter', function() {
+    return {
+        restrict: 'A',
+        link: function(scope, elem, attrs) {
+            // stuff goes here
+        }
+    };
+});
+
