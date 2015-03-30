a7199e7bdf688cca21eb0882494fea36f64b203c
jtreleaven <jtreleaven262@gmail.com>
2015-03-30 01:09:41 -0400

    Preparing angular app for basic demo on heroku.

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jeff on 3/30/15.
+ * Web server for live demo and continued updates and testing.
+ */
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname));
+
+app.get('/', function(request, response) {
+    response.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
