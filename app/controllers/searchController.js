cb4c4a87eb4777fc71be8913419101a3f6094bf6
Jeff Kikel <mcrazy300@yahoo.com>
2015-04-13 15:44:26 -0400

    Added basic search files, breaks ip address though?

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -1,2 +1,16 @@
 // AngularJS stub file
-<
+var search = angular.module('search', []);
+
+search.filter('dataFilter', function()
+	{
+		return function(ipAddress, searchText){
+			return people.filter(function(person){
+				var found = false;
+				Object.keys(ipAddress).some(function(key){
+					found = person[key].search(regexp) > -1;
+					return found
+				})
+			})
+		}
+	}
+)
