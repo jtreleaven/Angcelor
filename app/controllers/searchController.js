d4af5c6517bb71723070e08b0a9f0e9517871b01
Jeff Kikel <mcrazy300@yahoo.com>
2015-04-14 18:04:02 -0400

    IP Address search update

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -3,14 +3,14 @@ var search = angular.module('search', []);
 
 search.filter('dataFilter', function()
 	{
-		return function(ipAddress, searchText){
-			return people.filter(function(person){
+		return function(ip_addrs, searchText){
+			return ip_addrs.filter(function(ip_addr){
 				var found = false;
-				Object.keys(ipAddress).some(function(key){
-					found = person[key].search(regexp) > -1;
+				Object.keys(ip_addr).some(function(key){
+					found = ip_addrs[key].search(regexp) > -1;
 					return found;
 				})
 			})
 		}
 	}
-)
+);
